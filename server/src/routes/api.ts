e701fe0ab33b14c741d1f1e2f4e2cb21b6598b0d
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2017-06-07 23:04:03 +0200

    Add typings to server

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,29 +1,29 @@
-import { Router } from 'express';
-import Word from '../models/wordModel';
+import { Router, Request, Response } from 'express';
+import WordModel, { Word } from '../models/wordModel';
 
-const router = Router();
+const router: Router = Router();
 
-router.get('/word/list', (req, res) => {
-  Word.find((err, words) => {
+router.get('/word/list', (req: Request, res: Response) => {
+  WordModel.find((err, words: Word[]) => {
     if (err) res.send(err);
 
     res.json(words);
   });
 });
 
-router.post('/word/add', (req, res) => {
+router.post('/word/add', (req: Request, res: Response) => {
   let newWord = req.body.word;
 
   if (!newWord.foreignWord && !newWord.nativeWord) {
     res.status(400);
     res.json({ "error": "Bad Data" });
   } else {
-    let word = new Word({
+    let word = new WordModel({
       foreignWord: newWord.foreignWord,
       nativeWord: newWord.nativeWord
     });
 
-    word.save((err, word) => {
+    word.save((err, word: Word) => {
       if (err) res.send(err);
 
       res.json(word);
