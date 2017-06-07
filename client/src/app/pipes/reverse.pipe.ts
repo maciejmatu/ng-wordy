12612bc4e8ea2c3625bdb4b9082837382d91cc3b
karszczak <daniel.karski@gmail.com>
2017-06-07 07:22:11 +0200

    add reverse observable pipe

---
+++ client/src/app/pipes/reverse.pipe.ts
@@ -0,0 +1,13 @@
+import {Pipe, PipeTransform} from "@angular/core";
+import {Observable} from "rxjs";
+import 'rxjs/add/operator/map';
+
+@Pipe({
+  name: 'reverse'
+})
+
+export class ReversePipe implements PipeTransform{
+  transform($value: Observable<Array<any>>){
+    return $value.map(item => item.slice().reverse());
+  }
+}
