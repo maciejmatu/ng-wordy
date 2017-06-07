a8dcbc1e1f4577183aa5b0f206ee1b4e15f3df62
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2017-06-07 09:48:54 +0200

    Setup redux

---
+++ client/src/app/store.ts
@@ -0,0 +1,19 @@
+import { Action } from 'redux';
+import { LearnActions } from './learn/learn.actions';
+
+export interface AppState {
+  langToggled: boolean;
+}
+
+export const INITIAL_STATE: AppState = {
+  langToggled: true
+}
+
+export function rootReducer(lastState: AppState, action: Action): AppState {
+  switch(action.type) {
+    case LearnActions.TOGGLE_LANG:
+      return { langToggled: !lastState.langToggled }
+    default:
+      return lastState;
+  }
+}
