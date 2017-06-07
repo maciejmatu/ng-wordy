3b72e7009b2e3327dc4c84cbff043fa653f40716
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2017-06-07 19:20:17 +0200

    Refactor server and add live reload

--- server/src/config/main.ts
+++ server/src/config/main.ts
@@ -1,6 +1,7 @@
-export = {
+export default {
     secret: '',
     database: process.env.DATABASE_URL || 'mongodb://user:1234@ds161121.mlab.com:61121/wordy',
-    port: process.env.PORT || 3000,
-    clientURL: process.env.CLIENT_URL || 'http://localhost:3000'
+    port: process.env.PORT || 9001,
+    serverHost: process.env.HOST || 'http://localhost',
+    clientURL: process.env.CLIENT_URL || 'http://localhost:9000'
 }
--- server/src/models/listWordModel.ts
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const WordSchema = new Schema({
-    foreignWord: String,
-    nativeWord: String
-},{ collection : 'listWord' });
-
-module.exports = mongoose.model('listWord', WordSchema);
