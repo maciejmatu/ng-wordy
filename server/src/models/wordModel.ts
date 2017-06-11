4741b78a0af384b5d3cd0349e4b846afc58b9a23
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2017-06-11 11:19:40 +0200

    Add linting to server

--- server/src/models/wordModel.ts
+++ server/src/models/wordModel.ts
@@ -1,24 +1,24 @@
 import { Document, Schema, model } from 'mongoose';
 
 export interface Word extends Document {
-    foreignWord: string,
-    nativeWord: string,
-    id: string
+  foreignWord: string,
+  nativeWord: string,
+  id: string
 }
 
 const WordSchema: Schema = new Schema({
-    foreignWord: {
-        type: String,
-        required: true
-    },
-    nativeWord: {
-        type: String,
-        required: true
-    },
-    id: {
-        type: String,
-        required: true
-    }
-},{ collection : 'words' });
+  foreignWord: {
+    type: String,
+    required: true
+  },
+  nativeWord: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: String,
+    required: true
+  }
+}, { collection : 'words' });
 
 export default model<Word>('Word', WordSchema);
