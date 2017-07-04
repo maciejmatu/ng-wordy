e5b126c036dfb21544e761ebac42ca9d27da4df0
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2017-07-04 16:06:01 +0200

    Remove DB link

--- server/src/config/main.ts
+++ server/src/config/main.ts
@@ -8,7 +8,7 @@ let config: {
 
 export default config = {
   secret: '',
-  database: process.env.DATABASE_URL || 'mongodb://user:1234@ds161121.mlab.com:61121/wordy',
+  database: process.env.DATABASE_URL,
   port: process.env.PORT || 9001,
   serverHost: process.env.HOST || 'http://localhost',
   clientURL: process.env.CLIENT_URL || 'http://localhost:9000'
