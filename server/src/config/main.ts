17bcfce5d1575a0084bd3f2807e30208f855c526
Piotr Korlaga <piotrkorlaga@gmail.com>
2017-12-17 15:37:05 +0100

    added mongodb improvments and types improvments

--- server/src/config/main.ts
+++ server/src/config/main.ts
@@ -8,7 +8,7 @@ let config: {
 
 export default config = {
   secret: '',
-  database: process.env.DATABASE_URL,
+  database: process.env.DATABASE_URL || 'mongodb://localhost:27017',
   port: process.env.PORT || 9001,
   serverHost: process.env.HOST || 'http://localhost',
   clientURL: process.env.CLIENT_URL || 'http://localhost:9000'
